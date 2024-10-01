f3ad89c9bf7a426fef1c02adc7524baab2960f5f
rajinweb <rajinweb@gmail.com>
2024-10-02 00:22:03 +0530

    Update db.js with env

--- db.js
+++ db.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoD connection URL
-const mongoURL = process.env.Mongodb_online; //'mongodb://localhost:27017/hotels' // Replace database name
+//const mongoURL = 'mongodb://localhost:27017/hotels' // Replace database name 
+const mongoURL= process.env.Mongodb_online;
 
 // Set db MongoDB connection
 mongoose.connect (mongoURL, {
