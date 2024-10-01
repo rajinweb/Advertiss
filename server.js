5872dace1c33f153f76d3a4a838c7c5713e9c2df
rajinweb <rajinweb@gmail.com>
2024-10-02 00:21:35 +0530

    Update server.js with env

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const db = require('./db');
-
+require('dotenv').config();
 const bodyParser= require('body-parser');
 app.use(bodyParser.json());
 
@@ -41,6 +41,7 @@ app.get('/', function (req, res) {
   res.send('welcome to my website')
 });
 
-app.listen(3000, ()=>{
+const PORT=process.env.PORT || 3000
+app.listen(PORT, ()=>{
     console.log('listening to port 3000');
 })
