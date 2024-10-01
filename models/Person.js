bec28d71eed8df13dc50b0822207a24c10e32091
rajinweb <rajinweb@gmail.com>
2024-10-01 21:12:41 +0530

    model Person

---
+++ models/Person.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const {Schema}=mongoose;
+const personSchema = new Schema({
+    name:{
+        type: String
+    },
+    age:{
+        type: Number
+    }, 
+    work:{
+        type: String,
+        enum:['chef', 'waiter', 'manager'],
+        required: true
+    },
+    mobile:{
+        type: Number,
+        required:true,
+    },
+    email:{
+        type: String,
+        unique:true,
+        required:true
+    },
+    address:{
+        type: String
+    },
+    salary:{
+        type: Number
+    }
+}, { collection: 'emp' });
+
+// create peron model
+const Person= mongoose.model('Person', personSchema);
+module.exports= Person
