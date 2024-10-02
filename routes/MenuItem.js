03dbcb02b71725b3201a0a913f24200f8e6a8eec
rajinweb <rajinweb@gmail.com>
2024-10-02 10:36:33 +0000

    menu route added

---
+++ routes/MenuItem.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const Person = require('../models/Person');
+const router = express.Router();
+
+router.post('/menu', async (req, res)=> {
+    try{
+      const data= req.body; // New MenuItem data from user
+      const newMenuItem= new MenuItem(data);
+      const response= await newMenuItem.save();
+      console.log('MenuItem data saved');
+      res.status(200).json(response);
+    }catch(err){
+      console.log('Error', err);
+      res.status(500).json({error: 'internal server error 500'})
+    }
+});
+
+router.get('/menu', async (req, res) =>{
+  try{
+      const data = await MenuItem.find();
+      console.log('data fetched'); 
+      res.status(200).json(data) ;
+    }catch(err){
+      console.log (err);
+      res.status(500).json({error:'Internal Server Error'});
+    }
+  })
+
+  module.exports=router;
--- server.js
+++ server.js
@@ -8,33 +8,10 @@ app.use(bodyParser.json());
 
 const MenuItem = require('./models/MenuItem');
 const personRoutes = require('./routes/Person');
+const menuRoutes = require('./routes/MenuItem');
 
 app.use('/person', personRoutes);
-
-app.post('/menu', async (req, res)=> {
-    try{
-      const data= req.body; // New MenuItem data from user
-      const newMenuItem= new MenuItem(data);
-      const response= await newMenuItem.save();
-      console.log('MenuItem data saved');
-      res.status(200).json(response);
-    }catch(err){
-      console.log('Error', err);
-      res.status(500).json({error: 'internal server error 500'})
-    }
-});
-
-app.get('/menu', async (req, res) =>{
-  try{
-      const data = await MenuItem.find();
-      console.log('data fetched'); 
-      res.status(200).json(data) ;
-    }catch(err){
-      console.log (err);
-      res.status(500).json({error:'Internal Server Error'});
-    }
-  })
-
+app.use('/menu', menuRoutes);
 
 
 app.get('/', function (req, res) {
